d122083b66614933dee74f69a2547c4289bc998c
coder_Q <55419399+PlayGuitar-CoderQ@users.noreply.github.com>
2022-04-05 00:34:51 +0800

    feat: ✨ 增加svelte模块, 建起谷歌插件模板

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { chromeExtension } from 'vite-plugin-chrome-extension';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: "src/manifest.json",
+    }
+  },
   plugins: [
     svelte(),
+    chromeExtension(),
   ]
 })
